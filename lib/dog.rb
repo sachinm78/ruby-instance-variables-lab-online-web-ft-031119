4ecf9cb04ec0fdee061b0fd9c28d686064d60dc8
Sachin Mahajan <sachinm78@gmail.com>
2019-03-18 19:25:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
-class Dog 
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+class Dog    #class = key word    ##Dog = class name
+  def name=(dog_name)   # name= is an instance method
+    @this_dogs_name = dog_name    #@this_dogs_name = instance variable
   end
   
-  def name 
-    @this_dogs_name
+  def name      #name -s an instance method
+    @this_dogs_name   # = instance variable
   end
 end
 
