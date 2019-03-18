c1c6b69c50d964b865ead49c1c2df3f5afc8b8d5
Sachin Mahajan <sachinm78@gmail.com>
2019-03-18 19:26:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,8 @@ end
 
 lassie = Dog.new
 lassie.name = "Lassie"
+
+
+#Here we've defined two instance methods, the name=, or "name equals" method, and the name method. The first method takes in an argument of a dog's name and sets that argument equal to a variable, this_dogs_name. The second method is responsible for reporting, or reading the name. The methods act as mechanisms to expose data from inside of the object to the outside world.
+
+#Our two methods therefore are responsible for "setting" and "getting" an individual dog's name.
